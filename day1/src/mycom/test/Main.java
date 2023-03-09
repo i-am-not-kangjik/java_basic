c427e46736b05093918cb67cba482f2d53d65961
Kangjik Kim <rkqqkb@gmail.com>
2023-03-09 17:42:34 +0900

    java day1

---
+++ day1/src/mycom/test/Main.java
@@ -0,0 +1,16 @@
+package mycom.test;
+
+public class Main {
+    public static void main(String[] args) {
+        // Data
+        int x = 30;     //pd.read_csv()
+        int y = 40;
+        char tmp = 'A';
+        String tmp2 = "BCD";
+        // 명령어들의 집합
+        int z = x + y;
+        System.out.println(z);
+        String tmp3 = tmp + tmp2;
+        System.out.println(tmp3);
+    }
+};
